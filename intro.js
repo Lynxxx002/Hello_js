fea4eacadaa146974a38a0b5b52248837667ce2e
“Bekee <emmyxlynx@gmail.com>
2026-03-14 15:23:28 +0100

    Add intro.js

---
+++ intro.js
@@ -0,0 +1,8 @@
+// Introduction
+const name = "Claude";
+const age = 2;
+const favouriteLanguage = "JavaScript";
+
+console.log(`Name: ${name}`);
+console.log(`Age: ${age}`);
+console.log(`Favourite Language: ${favouriteLanguage}`);
